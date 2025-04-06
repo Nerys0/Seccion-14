e4c7fd3152e835eb995397b7c8a4d4ad61591acc
Nerys0 <neryslopezdeangel@hotmail.com>
2025-04-06 18:34:50 -0500

    cambios

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',function(){
 const button=document.getElementById ('changeTextButton');
 button.addEventListener('click', function(){
     const introSection=document.getElementById('intro');
-    introSection.innerHTML='<p> El texto ha cambiado despues de hacer click en el button!!</>';
+    introSection.innerHTML='<p> El texto ha cambiado despues de hacer click en el button!!</p>';
 
 });
 
