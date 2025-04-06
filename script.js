5af9f2f9ce8ade9144170ad2232642bce597ad7a
Nerys0 <neryslopezdeangel@hotmail.com>
2025-04-06 18:08:44 -0500

    scrip.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',function(){
+const button=document.getElementById ('changeTextButton');
+button.addEventListener('click'; function(){
+    const introSection=document.getElementById('intro');
+    introSection.innerHTML='<p>El texto ha cambiado despues de hacer click en el boton!!<p>';
+};
+});
+
+  
+     
