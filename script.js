e7dfc260f45d27b89585edfc390c1f53552a5c8c
Nerys0 <neryslopezdeangel@hotmail.com>
2025-04-06 18:18:20 -0500

    cambios

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const button=document.getElementById ('changeTextButton');
 button.addEventListener('click', function(){
     const introSection=document.getElementById('intro');
     introSection.innerHTML='<p>El texto ha cambiado despues de hacer click en el boton!!<p>';
-    
-};
+
+});
 
 });
 
